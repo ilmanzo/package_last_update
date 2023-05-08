7d68a320fbbfa76277398d3be37e5d6f82faceb0
Abraham12611 <abraham.dahunsi@gmail.com>
2023-05-08 14:25:53 +0100

    OSC error handling

--- shouldIupdate_parallel.sh
+++ shouldIupdate_parallel.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #osc my packages
-for p in $(osc -A https://api.opensuse.org my packages | cut -d '/' -f 2)
+
+
+if ! command -v osc &> /dev/null; then
+  echo "Error: The 'osc' command is not available. Please install it and try again later."
+  exit 1
+fi
+
+packages=$(osc -A https://api.opensuse.org my packages 2>&1)
+
+if [[ "$packages" == *Name\ or\ service\ not\ known* ]]; then
+  echo "Unable to connect to the specified API URL. Please check the URL and your network connection."
+  exit 1
+fi
+
+for p in $(echo "$packages" | cut -d '/' -f 2)
 do 
   ./last_update $p &
 done
