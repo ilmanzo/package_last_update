7f38b8b7a08539687bec6de56dffb0fb709adaad
Andrea Manzini <ilmanzo@gmail.com>
2023-06-16 16:18:45 +0200

    small cosmetic fixes to make linter happy

--- last_update.py
+++ last_update.py
@@ -8,10 +8,10 @@ import requests
 from cmp_version import VersionString
 
 
-## TODO make these configurable via cli
+# TODO make these configurable via cli
 APIURL = "https://api.opensuse.org"
 MAINPROJECT = "openSUSE:Factory"
-###
+# 
 
 OSC_CMD = ['osc', '--apiurl', APIURL]
 
@@ -19,12 +19,14 @@ REPOLOGY_APIURL = 'https://repology.org/api/v1/project/'
 
 
 def exec_process(cmdline: list[str]):
+    "helper to execute external process"
     return subprocess.Popen(
         cmdline, stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE, encoding='utf8')
 
 
 def convert_to_epoch(timestamp):
+    "given a date string, returns time in epoch (seconds from 1/1/1970)"
     try:
         # Attempt to parse timestamp in the format 'Month Day Hour:Minute'
         timestamp_with_year = f'{timestamp} {datetime.now().year}'
@@ -44,6 +46,7 @@ def convert_to_epoch(timestamp):
 
 
 def get_last_changes(package: str):
+    "parse project to get last changes modification date"
     try:
         proc = exec_process(
             OSC_CMD+["ls", "-l", f"{MAINPROJECT}/{package}"])
@@ -56,10 +59,12 @@ def get_last_changes(package: str):
 
 
 def is_numeric(version: str) -> bool:
-    return re.search(r"^\d+", version)
+    "return true if given version is numeric"
+    return re.search(r"^\d+", version) is not None
 
 
 def get_obs_version(package: str) -> str:
+    "query spec file in obs for the package version"
     try:
         osc = exec_process(
             OSC_CMD+["cat", f"{MAINPROJECT}/{package}/{package}.spec"]
@@ -79,8 +84,8 @@ def get_obs_version(package: str) -> str:
 
 
 def filter_repo(items: list[dict], refversion: str) -> list[dict]:
-    # uses some heuristic to create a list of only entries with a newer version
-    # on any parsing issue, returns the original list
+    """uses some heuristic to create a list of only entries with a newer version
+       on any parsing issue, returns the original list"""
     try:
         refv = VersionString(refversion)
         return list(filter(lambda x: refv < VersionString(x['version']), items))
@@ -89,6 +94,7 @@ def filter_repo(items: list[dict], refversion: str) -> list[dict]:
 
 
 def is_newer_on_repology(package: str, refversion: str) -> int:
+    "query repology.org API to get same named packaged with newer version"
     try:
         response = requests.get(f"{REPOLOGY_APIURL}/{package}",timeout=30)
         results = [r for r in response.json() if r['status'] ==
@@ -103,6 +109,7 @@ def is_newer_on_repology(package: str, refversion: str) -> int:
 
 
 def main():
+    "program entry point"
     parser = argparse.ArgumentParser(
         prog='last update',
         description='tells you when a package was last updated',
@@ -113,26 +120,26 @@ def main():
         '-m', '--machine', help='Use machine processable output instead of human readable', 
         action='store_true')
     args = parser.parse_args()
-    outputStr = f"- {args.package} "
+    output_str = f"- {args.package} "
     changes = get_last_changes(args.package)
     if not changes:
-        outputStr += "Error in getting information. Does this package exist in Factory ?"
-        return outputStr
+        output_str += "Error in getting information. Does this package exist in Factory ?"
+        return output_str
     else:
         obs_version = get_obs_version(args.package)
         changes = ' '.join(changes)
         if args.machine:
             changes = convert_to_epoch(changes)
-        outputStr += f"on {MAINPROJECT} is {obs_version} changed on {changes}"
+        output_str += f"on {MAINPROJECT} is {obs_version} changed on {changes}"
 
     repo_with_new_packages = is_newer_on_repology(args.package, obs_version)
     if is_numeric(obs_version) and repo_with_new_packages:
         if args.machine:
-            outputStr += " "
+            output_str += " "
         else:
-            outputStr += "\n"
-        outputStr += f"Other {repo_with_new_packages} repos may have newer versions"
-    return outputStr
+            output_str += "\n"
+        output_str += f"Other {repo_with_new_packages} repos may have newer versions"
+    return output_str
 
 
 # when imported as module, do not run
